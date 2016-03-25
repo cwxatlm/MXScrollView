557c0c9f8e3e090fab3f566676aa65a65c3fab21
max <cwx079@126.com>
2016-03-25 15:37:22 +0800

    修改配置

---
+++ MXScrollFramework/MXScrollView.h
@@ -0,0 +1,17 @@
+//
+//  MXScroll.h
+//  MXScrollViewDemo
+//
+//  Created by apple on 16/3/25.
+//  Copyright © 2016年 PRO. All rights reserved.
+//
+
+#ifndef MXScroll_h
+#define MXScroll_h
+
+#import "MXEnumHeader.h"
+#import "MXBaseScrollView.h"
+#import "MXPageControl.h"
+#import "MXImageView.h"
+
+#endif /* MXScroll_h */
